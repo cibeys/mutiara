80c85e9ed68b3081d26d463ecc9a512987b2f4d7
cibeys <132761045+cibeys@users.noreply.github.com>
2025-06-15 22:45:14 +0700

    update main.js

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
 
 /*====  efekmengetik  ====*/
 const dynamicText = document.querySelector("h2 span");
-const words = ["Mutiara Azika", "Mutiara Azika", "Mutiara Azika", "Mutiara Azika"];
+const words = ["Mutiara Azkia", "Mutiara Azkia", "Mutiara Azkia", "Mutiara Azkia"];
 // Variables to track the position and deletion status of the word
 let wordIndex = 0;
 let charIndex = 0;
